346f0ff7c7aaf07d6bd45dcf4a147df83fc98347
seyoung8239 <seyoung8239@gmail.com>
2022-08-24 10:32:06 +0900

    Feat: 문의 get 추가

--- routes/estimationRouter.js
+++ routes/estimationRouter.js
@@ -4,10 +4,15 @@ import { Estimation } from "../model/Estimation.js";
 const estimationRouter = express.Router();
 
 estimationRouter.post("/", (req, res) => {
-    const est = new Estimation(req.body);
+    const estimation = new Estimation(req.body);
 
-    est.save();
+    estimation.save();
     res.status(201).send("inquiry registered");
+});
+
+estimationRouter.get("/", async (req, res) => {
+    const allEstimation = await Estimation.find({});
+    res.status(200).json(allEstimation);
 });
 
 export default estimationRouter;
